8709707f82b08669b4ec40df58afbef439be43a9
Meredith Reece <mardebugger@gmail.com>
2018-01-08 02:45:39 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.string :description
       t.timestamps null: false
     end
